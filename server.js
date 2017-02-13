02a95698ba63978dd3acf69d3a1de9deac3e73c8
System <none>
2017-02-13 22:08:29 +0000

    second push

---
+++ server.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+// this will load our .env file if we're
+// running locally. On Gomix, .env files
+// are automatically loaded.
+require('dotenv').config();
+
+
+const {ALERT_FROM_EMAIL, ALERT_FROM_NAME, ALERT_TO_EMAIL} = process.env;
+const {logger} = require('./utilities/logger');
+const {sendEmail} = require('./emailer');
+// these are custom errors we've created
+const {FooError, BarError, BizzError} = require('./errors');
+
+const app = express();
+
+// this route handler randomly throws one of `FooError`,
+// `BarError`, or `BizzError`
+const russianRoulette = (req, res) => {
+  const errors = [FooError, BarError, BizzError];
+  throw new errors[
+    Math.floor(Math.random() * errors.length)]('It blew up!');
+};
+
+
+app.use(morgan('common', {stream: logger.stream}));
+
+// for any GET request, we'll run our `russianRoulette` function
+app.get('*', russianRoulette);
+
+const emailAlerts = (err, req, res, next) => {
+  if (err instanceof FooError || err instanceof BarError){
+    logger.info(`Sending error email to ${ALERT_TO_EMAIL}`);
+    
+    const emailData = {
+      from: ALERT_FROM_EMAIL,
+      to: ALERT_TO_EMAIL,
+      subject: `MY ALERT: ${err.name}`,
+      text: `Something went terribly wrong. All we know is this:\n\n${err.stack} + ${err.message}`,
+      html: '<p>This is the body of the html page of the error message that you recieved.</p>'
+    };
+    sendEmail(emailData);
+  }
+  next();
+}
+
+app.use(emailAlerts);
+
+
+
+
+// YOUR MIDDLEWARE FUNCTION should be activated here using
+// `app.use()`. It needs to come BEFORE the `app.use` call
+// below, which sends a 500 and error message to the client
+
+app.use((err, req, res, next) => {
+  logger.error(err);
+  res.status(500).json({error: 'Something went wrong'}).end();
+});
+
+const port = process.env.PORT || 8080;
+
+const listener = app.listen(port, function () {
+  logger.info(`Your app is listening on port ${port}`);
+});
